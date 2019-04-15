622c93d6a56770803924a832ff8dacd00c38ff05
brownguo <yexuan.guo@codedance.cn>
2019-04-15 15:45:24 +0800

    randstring

---
+++ bin/random_string.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guoyexuan
+ * Date: 2019/4/15
+ * Time: 3:35 PM
+ */
+
+class random_string
+{
+    public static function getRandomString($len)
+    {
+        $lower    = range('a','z');
+        $upper    = range('A','Z');
+        $digits   = range(0,9);
+
+        $chars = array_merge($lower,$upper,$digits);
+
+        shuffle($chars);
+
+        $randstring = '';
+
+        for($i = 0; $i < $len; $i++)
+        {
+            $randstring .= $chars[mt_rand(0,count($chars))];
+        }
+
+        return $randstring;
+    }
+}
+
+$start = random_string::getRandomString(16);
+
+print_r($start);
