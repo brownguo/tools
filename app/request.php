8103c535a8e86168c4418bbb334ddcade443d1d1
brownguo <yexuan.guo@codedance.cn>
2019-04-19 15:13:10 +0800

    fix

--- app/request.php
+++ app/request.php
@@ -11,11 +11,11 @@
 include_once "./pb/GetLoginQRCodeRequest.php";
 include_once "./pb/AesKey.php";
 include_once "./pb/BaseRequest.php";
-
+include_once "../bin/requests.php";
 $start = new GetLoginQRCodeRequest();
 $aeskey = new AesKey();
-$aeskey->setKey('123456');
-$aeskey->setLen(6);
+$aeskey->setKey('hQvyCVmcLt98WBUf');
+$aeskey->setLen(16);
 
 //$start->setAes($aeskey->setKey('JumpManAesKeys!'));
 //$start->setBaseRequest('111');
@@ -29,11 +29,11 @@ $aeskey->setLen(6);
 
 $baseR = new BaseRequest();
 
-$baseR->setSessionKey('Session1Key');
+$baseR->setSessionKey('hQvyCVmcLt98WBUf');
 $baseR->setUin('0');
-$baseR->setDevicelId('1231231ddd');
-$baseR->setClientVersion('12313');
-$baseR->setOsType('iPhone');
+$baseR->setDevicelId('2/!\000\n\235\372\263\014B\306v\275\257\337\001');
+$baseR->setClientVersion('369558056');
+$baseR->setOsType('iPad iPhone OS9.3.3');
 $baseR->setScene(0);
 
 //print_r($baseR->dump());
@@ -43,12 +43,29 @@ $baseR->setScene(0);
 //print_r($dump_all_filed);
 
 
-$start->setAes();
+print_r($baseR->values);
 $start->setBaseRequest($baseR);
 $start->setAes($aeskey);
 $start->setOpcode(0);
 $start->setExtDevLoginType(0);
+print_r($start->getBaseRequest());
+exit();
 
+//$packed = $start->encode();
+
+
+//$url = 'http://short.weixin.qq.com/cgi-bin/micromsg-bin/getloginqrcode';
+//$res = requests::post($url,$packed,null);
+//var_dump($res);
+//to 16
+//print_r(bin2hex($packed));
+//print_r($packed);
+
+
+
+
+
+//print_r($start->getBaseRequest());
 //$fields = $start->fields();
 //foreach ($fields as $key => $_item) {
 //    $field_name = $_item['name'];
@@ -57,5 +74,6 @@ $start->setExtDevLoginType(0);
 //    }
 //}
 
-print_r($start->fields());
 print_r($start->dump());
+//print_r($packed);
+//print_r($start->serializeToString());
