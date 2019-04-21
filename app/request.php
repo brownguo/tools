d58ea1d9f50c323cc482c42b6d464222a79e6c23
brownguo <yexuan.guo@codedance.cn>
2019-04-21 18:39:19 +0800

    buffer

--- app/request.php
+++ app/request.php
@@ -12,21 +12,38 @@ include_once "./pb/GetLoginQRCodeRequest.php";
 include_once "./pb/AesKey.php";
 include_once "./pb/BaseRequest.php";
 include_once "../bin/requests.php";
-$start = new GetLoginQRCodeRequest();
+
+
+
+/*==================AesKey===========================*/
 $aeskey = new AesKey();
 $aeskey->setKey('hQvyCVmcLt98WBUf');
 $aeskey->setLen(16);
+//print_r($aeskey->dump());
+/*==================AesKey===========================*/
 
-$baseRequest = new BaseRequest();
 
 
 
 /*==================BaseRequest===========================*/
+$baseRequest = new BaseRequest();
 $baseRequest->setSessionKey('hQvyCVmcLt98WBUf');
 $baseRequest->setUin(0);
-$baseRequest->setDevicelId('\377\270y\014\373\236\237\257h\\a\010(\005?\036');
+$baseRequest->setDevicelId('c_\027\016].7_\237\324\010\306Ke,\013');
 $baseRequest->setClientVersion('369558056');
 $baseRequest->setOsType('iPad iPhone OS9.3.3');
 $baseRequest->setScene(0);
-print_r($baseRequest->dump());
+//print_r($baseRequest->dump());
 /*==================BaseRequest===========================*/
+
+
+/*==================GetLoginQRCodeRequest===========================*/
+
+$start = new GetLoginQRCodeRequest();
+$start->setBaseRequest($baseRequest);
+$start->setAes($aeskey);
+$start->setOpcode(0);
+$start->setExtDevLoginType(0);
+$packed = $start->getBaseRequest()->serializeToString();
+$start->dump();
+print_r($packed);
