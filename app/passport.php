b4b4a72913c74695cba9bf045a7e221bcd4b5033
brownguo <yexuan.guo@codedance.cn>
2019-05-29 17:08:34 +0800

    pb

---
+++ app/passport.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guoyexuan
+ * Date: 2019/5/29
+ * Time: 下午2:46
+ */
+
+
+
+include_once "./pb/BaseRequest.php";
+include_once "./pb/GetLoginQRCodeRequest.php";
+
+$baseRequest = array(
+    'baseRequst' => array(
+        'sessionKey'   => 'k2J7XS4cIALnU6Ru',
+        'uin'           => 0,
+        'devicelId'     => '\301\004\313P\234\303\371 I\261\241\234<O\242r',
+        'clientVersion' => '369558056',
+        'osType'        => 'iPad iPhone OS9.3.3',
+        'scene'         => 0
+    ),
+    'aes' => array(
+        'len'   =>16,
+        'key'   =>"k2J7XS4cIALnU6Ru"
+    ),
+    'opcode'         =>0,
+    'extDevLoginType'=>0
+);
+
+
+$baseRequest = new BaseRequest();
+$baseRequest->setSessionKey('hQvyCVmcLt98WBUf');
+$baseRequest->setUin(0);
+$baseRequest->setDevicelId('c_\027\016].7_\237\324\010\306Ke,\013');
+$baseRequest->setClientVersion('369558056');
+$baseRequest->setOsType('iPad iPhone OS9.3.3');
+$baseRequest->setScene(0);
+
+$start = new GetLoginQRCodeRequest();
+$start->setBaseRequest($baseRequest);
+
+print_r($start->SerializeToString());
+#print_r(json_encode($baseRequest));
