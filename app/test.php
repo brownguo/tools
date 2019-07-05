8d81e79d6805ba734618c51a83c464910ad92ff0
brownguo <yexuan.guo@codedance.cn>
2019-07-05 16:56:26 +0800

    test

--- app/test.php
+++ app/test.php
@@ -134,43 +134,55 @@ if("\x00" == 0x00)
 //print_r(AToHex(125));
 //echo PHP_EOL;
 
-include "../bin/requests.php";
+//include "../bin/requests.php";
+//
+//$url = 'http://mail.std.uestc.edu.cn/coremail/s/json?func=user%3AsetAttrs&sid=BAmhMaFFRUtQVZczgVFFVcHsBeTmbdRa';
+//
+//$send_data = array(
+//    'attrs'=>array(
+//        "true_name"     =>  "张凯",
+//        "alt_email"     =>  "",
+//        "nick_name"     =>  "zkay",
+//        "gender"        =>  "0",
+//        "classes"       =>  "2013060110",
+//        "subject"       =>  "计算机科学与技术",
+//        "campus"        =>  "清水河校区",
+//        "room_number"   =>  "16-8-4",
+//        "qq_number"     =>  "1480630390",
+//        "wechat_number" =>  "",
+//        "remarks"       =>  ""
+//    ),
+//);
+//
+//$header = array(
+//        'Accept'            =>'text/x-json',
+//        'Accept-Encoding'   =>'gzip, deflate',
+//        'Accept-Language'   =>'zh-CN,zh;q=0.9,en;q=0.8',
+//        'Cache-Control'     =>'no-cache',
+//        'Connection'        =>'keep-alive',
+//        'Content-Length'    =>strlen($send_data),
+//        'Content-Type'      =>'text/x-json',
+//        'Cookie'            =>'face=auto; locale=zh_CN; saveUsername=true; domain=std.uestc.edu.cn; CoremailReferer=http%3A%2F%2Fmail.std.uestc.edu.cn%2F; UM_distinctid=16bb13c3654385-057758ec204684-37677e05-1fa400-16bb13c3655664; uid=2013060110028%40std.uestc.edu.cn; Coremail=fb10217e55f63d974194b3a46d89375f; Coremail.sid=BAmhMaFFRUtQVZczgVFFVcHsBeTmbdRa',
+//        'Host'              =>'mail.std.uestc.edu.cn',
+//        'Origin'            =>'http://mail.std.uestc.edu.cn',
+//        'Pragma'            =>'no-cache',
+//        'Referer'           =>'http://mail.std.uestc.edu.cn/coremail/XT5/index.jsp?sid=BAmhMaFFRUtQVZczgVFFVcHsBeTmbdRa',
+//        'User-Agent'        =>'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+//        'X-Requested-With'  =>'XMLHttpRequest',
+//);
+//
+//$res = requests::post($url,$send_data,$header,false,false,null);
+//
+//print_r($res);
 
-$url = 'http://mail.std.uestc.edu.cn/coremail/s/json?func=user%3AsetAttrs&sid=BAmhMaFFRUtQVZczgVFFVcHsBeTmbdRa';
+$html = file_get_contents('https://www.adidas.com.cn/item/EF2778?locale=zh_CN');
 
-$send_data = array(
-    'attrs'=>array(
-        "true_name"     =>  "张凯",
-        "alt_email"     =>  "",
-        "nick_name"     =>  "zkay",
-        "gender"        =>  "0",
-        "classes"       =>  "2013060110",
-        "subject"       =>  "计算机科学与技术",
-        "campus"        =>  "清水河校区",
-        "room_number"   =>  "16-8-4",
-        "qq_number"     =>  "1480630390",
-        "wechat_number" =>  "",
-        "remarks"       =>  ""
-    ),
-);
 
-$header = array(
-        'Accept'            =>'text/x-json',
-        'Accept-Encoding'   =>'gzip, deflate',
-        'Accept-Language'   =>'zh-CN,zh;q=0.9,en;q=0.8',
-        'Cache-Control'     =>'no-cache',
-        'Connection'        =>'keep-alive',
-        'Content-Length'    =>strlen($send_data),
-        'Content-Type'      =>'text/x-json',
-        'Cookie'            =>'face=auto; locale=zh_CN; saveUsername=true; domain=std.uestc.edu.cn; CoremailReferer=http%3A%2F%2Fmail.std.uestc.edu.cn%2F; UM_distinctid=16bb13c3654385-057758ec204684-37677e05-1fa400-16bb13c3655664; uid=2013060110028%40std.uestc.edu.cn; Coremail=fb10217e55f63d974194b3a46d89375f; Coremail.sid=BAmhMaFFRUtQVZczgVFFVcHsBeTmbdRa',
-        'Host'              =>'mail.std.uestc.edu.cn',
-        'Origin'            =>'http://mail.std.uestc.edu.cn',
-        'Pragma'            =>'no-cache',
-        'Referer'           =>'http://mail.std.uestc.edu.cn/coremail/XT5/index.jsp?sid=BAmhMaFFRUtQVZczgVFFVcHsBeTmbdRa',
-        'User-Agent'        =>'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        'X-Requested-With'  =>'XMLHttpRequest',
-);
+print_r($html);
 
-$res = requests::post($url,$send_data,$header,false,false,null);
+preg_match_all('/<li ipi="(.*?)" size="6-" dispalysize="40 (UK6½)">
+															<a>
+																40<\/a>
+														<\/li>/im',$html,$size_sku);
 
-print_r($res);
+print_r($size_sku);
