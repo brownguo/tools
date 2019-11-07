89b6d82ce2478144eab651408979a90ed85a023a
brownguo <yexuan.guo@codedance.cn>
2019-11-07 18:38:26 +0800

    fixed

--- snkrs_go.py
+++ snkrs_go.py
@@ -10,12 +10,15 @@ import sys
 #sys.exit(0)
 #创建浏览器对象
 
-while 0 < 1000:
+print('启动时间:',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+i = 0
+while i < 1000:
     driver = webdriver.Chrome()
     # 窗口最大化显示
     driver.maximize_window()
     driver.set_window_size(200, 200)
-    driver.get('https://www.nike.com/cn/')
+    driver.get('https://baidu.com/')
 
 
 time.sleep(30)
